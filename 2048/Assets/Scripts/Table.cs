de762b5f321cd3fe89cacbd229e4b63a92e26a2a
Agustin Rosso <60651699+Hazard103508@users.noreply.github.com>
2021-08-01 01:42:36 -0300

    .
    .

---
+++ 2048/Assets/Scripts/Table.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MonoBehaviour
+{
+    [Header("Prefabs")]
+    public List<GameObject> Blocks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
